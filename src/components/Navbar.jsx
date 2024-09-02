c43575376efddec3beae17b69df13b874bfcd951
Izhann <izhan124@gmail.com>
2024-09-03 01:09:47 +0500

    commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,111 +1,3 @@
-// // // // import React, { useState } from 'react'
-// // // // import { Link } from 'react-scroll'
-// // // // import { Link as RouteLink } from 'react-router-dom'
-// // // // import sponge from '../assets/sponge.png'
-// // // // import { RiMenu4Fill } from 'react-icons/ri';
-// // // // import { MdArrowOutward } from "react-icons/md";
-// // // // import { MdOutlineAlternateEmail } from "react-icons/md";
-// // // // import { RxHamburgerMenu, RxCross1 } from "react-icons/rx";
-// // // // import { FaTwitter, FaInstagram, FaLinkedin, FaCross } from 'react-icons/fa';
-// // // // import { AnimatePresence, motion } from 'framer-motion';
-// // // // import logo from '../assets//logos/7-2.png';
-// // // // import DS1 from '../assets/DS1.png';
-// // // // import { FaX } from 'react-icons/fa6';
-// // // // import { CgClose } from 'react-icons/cg';
-
-// // // // const Navbar = () => {
-// // // //   const [menuOpen, setMenuOpen] = useState(false);
-
-// // // //   const toggleMenu = () => {
-// // // //     setMenuOpen(!menuOpen);
-// // // //   };
-
-// // // //   return (
-// // // //     <div className={`max-w-full w-full h-16 border-t-0 border-b-2 border-black flex justify-between items-center tracking-wide fixed z-50 `}>
-// // // //       <div className={`flex h-full ${!menuOpen ? 'w-full backdrop-blur-md' : ''}`}>
-
-// // // //         <div onClick={toggleMenu} className={`${!menuOpen ? 'bg-[#ffd21d] text-white' : 'bg-transparent text-black border-b-2'} text-3xl font-bold h-full w-16 grid place-items-center hover:text-black hover:bg-white transition-all duration-500 border-r-2  border-black z-50`}>
-// // // //           <div>{menuOpen ? <CgClose /> : <RiMenu4Fill />}</div>
-// // // //         </div>
-
-// // // //         <RouteLink to={'/'}> <div className='text-xl p-2 text-black font-bold h-full w-max grid place-items-center hover:text-[#fccb0d] transition-all duration-500 border-r-2 border-black text-center leading-none '>
-// // // //           {/* <div>Digtal<br/>Sponge.</div> */}
-// // // //           <img src={logo} className='w-40 mix-blend-difference' alt="" />
-// // // //         </div></RouteLink>
-
-
-// // // //       </div>
-
-// // // //       <div className='text-sm lg:text-md grid px-5 lg:px-10 md:border-l-2 border-black h-full w-48 lg:w-40  place-items-center backdrop-blur-md font-semibold hover:bg-[#fccb0d] hover:text-white duration-500 transition-all'>
-// // // //         <RouteLink to={'/payment'}>    <p className=' '>PAY NOW
-// // // //         </p>
-// // // //         </RouteLink>
-// // // //       </div>
-// // // //       <AnimatePresence>
-// // // //         {menuOpen && (
-// // // //           <motion.div
-// // // //             className={`overlay-menu fixed inset-0 bg-white text-black flex flex-row items-center justify-start pt-10 lg:pt-0 z-20  text-left max-h-screen`}
-// // // //             initial={{ y: "-100%", opacity: 0 }}
-// // // //             animate={{ y: "0%", opacity: 1 }}
-// // // //             exit={{ y: "-100%", opacity: 0 }}
-// // // //             transition={{ duration: 0.5, ease: "easeInOut" }}
-// // // //             key="menu"
-// // // //           >
-// // // //             <div className="w-full lg:w-1/2 pl-0 lg:pl-20 max-h-screen ">
-// // // //               <ul className="text-2xl h-full flex flex-col justify-evenly font-bold lg:font-semibold">
-// // // //                 <li className='overlay-menu-item text-5xl lg:text-7xl'>
-// // // //                   <Link to='home' onClick={toggleMenu}>Home</Link>
-// // // //                   <div className='overlay-menu-arrow'>
-// // // //                     <MdArrowOutward size={50} className=' bg-black rounded-full text-white p-1' />
-// // // //                   </div>
-// // // //                 </li>
-// // // //                 <hr className='w-full border-stone-500' />
-// // // //                 <li className='overlay-menu-item text-5xl lg:text-7xl'>
-// // // //                   <Link to='about' onClick={toggleMenu} smooth={true} duration={500}>About</Link>
-// // // //                   <div className='overlay-menu-arrow'>
-// // // //                     <MdArrowOutward size={50} className=' bg-black rounded-full text-white p-1' />
-// // // //                   </div>
-// // // //                 </li>
-// // // //                 <hr className='w-full border-stone-500' />
-// // // //                 <li className='overlay-menu-item text-5xl lg:text-7xl'>
-// // // //                   <Link to='work' onClick={toggleMenu} smooth={true} duration={500}>Work</Link>
-// // // //                   <div className='overlay-menu-arrow'>
-// // // //                     <MdArrowOutward size={50} className=' bg-black rounded-full text-white p-1' />
-// // // //                   </div>
-// // // //                 </li>
-// // // //                 <hr className='w-full border-stone-500' />
-// // // //                 <li className='overlay-menu-item text-5xl lg:text-7xl'>
-// // // //                   <Link to='/#services' onClick={toggleMenu} smooth={true} duration={500}>Services</Link>
-// // // //                   <div className='overlay-menu-arrow'>
-// // // //                     <MdArrowOutward size={50} className=' bg-black rounded-full text-white p-1' />
-// // // //                   </div>
-// // // //                 </li>
-// // // //                 <hr className='w-full border-stone-500' />
-// // // //                 <li className='overlay-menu-item text-5xl lg:text-7xl'>
-// // // //                   <Link to='process' onClick={toggleMenu} smooth={true} duration={500}>Process</Link>
-// // // //                   <div className='overlay-menu-arrow'>
-// // // //                     <MdArrowOutward size={50} className=' bg-black rounded-full text-white p-1' />
-// // // //                   </div>
-// // // //                 </li>
-// // // //                 <hr className='w-full border-stone-500' />
-// // // //                 <li className='overlay-menu-item text-5xl lg:text-7xl'>
-// // // //                   <RouteLink to='/contact' onClick={toggleMenu} smooth={true} duration={500}>Contact</RouteLink>
-// // // //                   <div className='overlay-menu-arrow'>
-// // // //                     <MdArrowOutward size={50} className=' bg-black rounded-full text-white p-1' />
-// // // //                   </div>
-// // // //                 </li>
-// // // //               </ul>
-// // // //             </div>
-
-// // // //           </motion.div>
-// // // //         )}
-// // // //       </AnimatePresence>
-// // // //     </div>
-// // // //   )
-// // // // }
-
-// // // // export default Navbar
-
 import React, { useState, useEffect } from 'react';
 import { Link as ScrollLink, scroller } from 'react-scroll';
 import { Link as RouteLink, useNavigate, useLocation } from 'react-router-dom';
@@ -113,27 +5,54 @@ import { RiMenu4Fill } from 'react-icons/ri';
 import { MdArrowOutward } from "react-icons/md";
 import { CgClose } from 'react-icons/cg';
 import { AnimatePresence, motion } from 'framer-motion';
-import logo from '../assets/logos/7-2.png';
+import logoIcon from '../assets/logos/logo-icon.png';
+import logoText from '../assets/logos/logo-text.png';
+
 
 const Navbar = () => {
   const [menuOpen, setMenuOpen] = useState(false);
-  const [targetSection, setTargetSection] = useState(null);
+  const [isDarkSectionVisible, setIsDarkSectionVisible] = useState(false);
   const navigate = useNavigate();
   const location = useLocation();
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        const entry = entries[0];
+        if (entry.isIntersecting) {
+          setIsDarkSectionVisible(true);
+        } else {
+          setIsDarkSectionVisible(false);
+        }
+      },
+      {
+        threshold: 0.1, // Adjust this threshold as needed
+        rootMargin: '0px 0px -70% 0px', // Adjust the root margin if needed
+      }
+    );
+
+    const darkSection = document.getElementById('process');
+    if (darkSection) {
+      observer.observe(darkSection);
+    }
+
+    return () => {
+      if (darkSection) {
+        observer.unobserve(darkSection);
+      }
+    };
+  }, []);
 
   const toggleMenu = () => {
     setMenuOpen(!menuOpen);
   };
 
   const handleNavigation = (to) => {
-    setTargetSection(to);
     if (location.pathname !== '/') {
-      // If not on the homepage, navigate to the homepage first
       navigate('/');
     } else {
-      // If on the homepage, scroll to the section directly
       scrollToSection(to);
-      toggleMenu(); // Close the menu after navigation
+      toggleMenu();
     }
   };
 
@@ -143,93 +62,85 @@ const Navbar = () => {
         smooth: true,
         duration: 500,
       });
-    }, 100); // Add a slight delay (100ms)
+    }, 100);
   };
 
-  // Handle the case when you navigate to the homepage from another page
-  useEffect(() => {
-    if (location.pathname === '/' && targetSection) {
-      scrollToSection(targetSection);
-      setTargetSection(null); // Reset targetSection after scrolling
-    }
-  }, [location, targetSection]);
-
   return (
-    <div className={`max-w-full w-full h-16 border-t-0 border-b-2 border-black flex justify-between items-center tracking-wide fixed z-50 `}>
+    <div className={`max-w-full w-full h-16 border-t-0 border-b-2 border-black flex justify-between items-center tracking-wide fixed z-50`}>
       <div className={`flex h-full ${!menuOpen ? 'w-full backdrop-blur-md' : ''}`}>
         <div onClick={toggleMenu} className={`${!menuOpen ? 'bg-[#ffd21d] text-white' : 'bg-transparent text-black border-b-2'} text-3xl font-bold h-full w-16 grid place-items-center hover:text-black hover:bg-white transition-all duration-500 border-r-2 border-black z-50`}>
           <div>{menuOpen ? <CgClose /> : <RiMenu4Fill />}</div>
         </div>
 
-        <RouteLink to={'/'}> 
-          <div className='text-xl p-2 text-black font-bold h-full w-max grid place-items-center hover:text-[#fccb0d] transition-all duration-500 border-r-2 border-black text-center leading-none '>
-            <img src={logo} className='w-40 mix-blend-difference' alt="" />
+        <RouteLink to={'/'}>
+          <div className='text-xl p-2 text-black font-bold h-full w-max flex hover:text-[#fccb0d] transition-all duration-500 border-r-2 border-black text-center leading-none'>
+          <img src={logoIcon} className={`w-20 object-contain ${isDarkSectionVisible ? '' : ''}`} alt="Logo" />
+          <img src={logoText} className={`w-20 object-contain ${isDarkSectionVisible ? 'inverted-logo' : ''}`} alt="Logo" />
           </div>
         </RouteLink>
       </div>
 
-      <div className='text-sm lg:text-md grid px-5 lg:px-10 md:border-l-2 border-black h-full w-48 lg:w-40 place-items-center backdrop-blur-md font-semibold hover:bg-[#fccb0d] hover:text-white duration-500 transition-all'>
-        <RouteLink to={'/payment'}>    
-          <p className=''>PAY NOW</p>
+      <div className={`text-sm lg:text-md grid px-5 lg:px-10 md:border-l-2 border-black h-full w-48 lg:w-40 place-items-center backdrop-blur-md font-semibold hover:bg-[#fccb0d] hover:text-white duration-500 transition-all `}>
+        <RouteLink to={'/payment'}>
+          <p className={`${isDarkSectionVisible ? 'inverted-logo' : ''}`}>PAY NOW</p>
         </RouteLink>
       </div>
 
       <AnimatePresence>
         {menuOpen && (
           <motion.div
-            className={`overlay-menu fixed inset-0 bg-white text-black flex flex-row items-center justify-start pt-10 lg:pt-0 z-20  text-left max-h-screen`}
+            className={`overlay-menu fixed inset-0 bg-white text-black flex flex-row items-center justify-start pt-10 lg:pt-0 z-20 text-left max-h-screen`}
             initial={{ y: "-100%", opacity: 0 }}
             animate={{ y: "0%", opacity: 1 }}
             exit={{ y: "-100%", opacity: 0 }}
             transition={{ duration: 0.5, ease: "easeInOut" }}
             key="menu"
           >
-            <div className="w-full lg:w-1/2 pl-0 lg:pl-20 max-h-screen ">
+            <div className="w-full lg:w-1/2 pl-0 lg:pl-20 max-h-screen">
               <ul className="text-2xl h-full flex flex-col justify-evenly font-bold lg:font-semibold">
                 <li className='overlay-menu-item text-5xl lg:text-7xl'>
                   <ScrollLink to='home' onClick={() => handleNavigation('home')} smooth={true} duration={500}>Home</ScrollLink>
                   <div className='overlay-menu-arrow'>
-                    <MdArrowOutward size={50} className=' bg-black rounded-full text-white p-1' />
+                    <MdArrowOutward size={50} className='bg-black rounded-full text-white p-1' />
                   </div>
                 </li>
                 <hr className='w-full border-stone-500' />
                 <li className='overlay-menu-item text-5xl lg:text-7xl'>
                   <ScrollLink to='about' onClick={() => handleNavigation('about')} smooth={true} duration={500}>About</ScrollLink>
                   <div className='overlay-menu-arrow'>
-                    <MdArrowOutward size={50} className=' bg-black rounded-full text-white p-1' />
+                    <MdArrowOutward size={50} className='bg-black rounded-full text-white p-1' />
                   </div>
                 </li>
                 <hr className='w-full border-stone-500' />
                 <li className='overlay-menu-item text-5xl lg:text-7xl'>
                   <ScrollLink to='work' onClick={() => handleNavigation('work')} smooth={true} duration={500}>Work</ScrollLink>
                   <div className='overlay-menu-arrow'>
-                    <MdArrowOutward size={50} className=' bg-black rounded-full text-white p-1' />
+                    <MdArrowOutward size={50} className='bg-black rounded-full text-white p-1' />
                   </div>
                 </li>
                 <hr className='w-full border-stone-500' />
                 <li className='overlay-menu-item text-5xl lg:text-7xl'>
                   <ScrollLink to='services' onClick={() => handleNavigation('services')} smooth={true} duration={500}>Services</ScrollLink>
                   <div className='overlay-menu-arrow'>
-                    <MdArrowOutward size={50} className=' bg-black rounded-full text-white p-1' />
+                    <MdArrowOutward size={50} className='bg-black rounded-full text-white p-1' />
                   </div>
                 </li>
                 <hr className='w-full border-stone-500' />
                 <li className='overlay-menu-item text-5xl lg:text-7xl'>
                   <ScrollLink to='process' onClick={() => handleNavigation('process')} smooth={true} duration={500}>Process</ScrollLink>
                   <div className='overlay-menu-arrow'>
-                    <MdArrowOutward size={50} className=' bg-black rounded-full text-white p-1' />
+                    <MdArrowOutward size={50} className='bg-black rounded-full text-white p-1' />
                   </div>
                 </li>
                 <hr className='w-full border-stone-500' />
                 <li className='overlay-menu-item text-5xl lg:text-7xl'>
                   <RouteLink to='/contact' onClick={toggleMenu}>Contact</RouteLink>
                   <div className='overlay-menu-arrow'>
-                    <MdArrowOutward size={50} className=' bg-black rounded-full text-white p-1' />
+                    <MdArrowOutward size={50} className='bg-black rounded-full text-white p-1' />
                   </div>
                 </li>
               </ul>
             </div>
-
           </motion.div>
         )}
       </AnimatePresence>
--- src/components/OurWork.jsx
+++ src/components/OurWork.jsx
@@ -1,102 +1,3 @@
-// import React, { useEffect, useRef, useState } from 'react';
-// import { motion, useAnimation } from 'framer-motion';
-// import { Link } from 'react-router-dom';
-// import whatwedo from '../assets/what-we-do-heading.png';
-// import illustration from '../assets/illustration.png';
-// import comics from '../assets/comics.png';
-// import emotes from '../assets/emotes.png';
-// import animation from '../assets/animations.png';
-
-// const OurWork = () => {
-//     const controls = useAnimation();
-//     const sectionRef = useRef(null);
-
-//     useEffect(() => {
-//         const observer = new IntersectionObserver(
-//             ([entry]) => {
-//                 if (entry.isIntersecting) {
-//                     controls.start('visible');
-//                 } else {
-//                     controls.start('hidden');
-//                 }
-//             },
-//             { threshold: 0.3 } // Trigger when 30% of the section is visible
-//         );
-
-//         if (sectionRef.current) {
-//             observer.observe(sectionRef.current);
-//         }
-
-//         return () => {
-//             if (sectionRef.current) {
-//                 observer.unobserve(sectionRef.current);
-//             }
-//         };
-//     }, [controls]);
-
-//     const listItemVariants = {
-//         hidden: { opacity: 0, y: 50 },
-//         visible: { opacity: 1, y: 0 },
-//     };
-
-//     const sectionVariants = {
-//         hidden: { opacity: 0 },
-//         visible: { opacity: 1 },
-//     };
-
-//     return (
-//         <motion.section
-//             key={1}
-//             ref={sectionRef}
-//             className='w-full py-10'
-//             initial="hidden"
-//             animate={controls}
-//             variants={sectionVariants}
-//             transition={{ duration: 0.5, ease: "easeInOut" }}
-//         >
-//             <div id='work' className='flex flex-col md:flex-row justify-between items-center '>
-//                 <img src={whatwedo} alt="What We Do" className='w-full md:w-2/5 mt-20 md:mt-10' />
-//                 <Link to={'/portfolio'}>
-//                     <button className='hidden md:block text-lg font-semibold px-4 py-3 bg-[#facb0d] mt-10 border border-black drop-shadow-[4px_4px_0px_#1f1f1f] hover:drop-shadow-[1px_1px_0px_#1f1f1f] transition-all duration-200 md:mr-10' style={{ WebkitTextStroke: '0px #1f1f1f', textShadow: 'none' }}>
-//                         Explore Full Portfolio
-//                     </button>
-//                 </Link>
-//             </div>
-
-//             <motion.div
-//                 className={`inset-0 text-black flex flex-row items-center justify-start pt-10 lg:pt-0 text-left`}
-//             >
-//                 <div className="w-full h-full p-10">
-//                     <ul className="text-2xl h-full flex flex-col justify-evenly font-bold lg:font-semibold">
-
-
-//                         {['Animations', 'Comics', 'Illustrations', 'Emotes'].map((item, index) => (
-//                             <div className='group' key={index}>
-//                                 <motion.li
-//                                     className='flex justify-between text-5xl lg:text-7xl px-5 py-10 transition-all duration-500 bg-white rounded-full my-5 hover:tracking-wider'
-//                                     variants={listItemVariants}
-//                                     transition={{ delay: 0.2 * index, duration: 0.5, ease: 'easeIn' }}
-//                                 >
-//                                     <p>{item}</p>
-//                                     <img className='mr-20 absolute right-0  hidden group-hover:block w-48 h-40  float-right object-cover ' src={item === 'Animations' ? animation : item === 'Illustrations' ? illustration : item === 'Comics' ? comics : item === 'Emotes' ? emotes : null} alt="" />
-//                                 </motion.li>
-//                             </div>
-//                         ))}
-//                     </ul>
-//                 </div>
-//             </motion.div>
-//             <Link to={'/portfolio'}>
-//                 <button className='block md:hidden text-lg font-semibold px-4 py-3 bg-[#facb0d] mt-10 border border-black drop-shadow-[4px_4px_0px_#1f1f1f] hover:drop-shadow-[1px_1px_0px_#1f1f1f] transition-all duration-200 ml-10' style={{ WebkitTextStroke: '0px #1f1f1f', textShadow: 'none' }}>
-//                     Explore Full Portfolio
-//                 </button>
-//             </Link>
-//         </motion.section>
-//     );
-// };
-
-// export default OurWork;
-
-
 import React, { useEffect, useRef, useState } from 'react';
 import { motion, useAnimation } from 'framer-motion';
 import { Link } from 'react-router-dom';
@@ -185,11 +86,11 @@ const OurWork = () => {
             <motion.div
                 className={`inset-0 text-black flex flex-row items-center justify-start pt-10 lg:pt-0 text-left`}
             >
-                <div className="w-full h-full p-10">
+                <div className="w-full h-full p-1 md:p-5 lg:p-10">
                     <ul className="text-2xl h-full flex flex-col justify-evenly font-bold lg:font-semibold">
                         {['Animations', 'Comics', 'Illustrations', 'Emotes'].map((item, index) => (
                             <Link key={index} to={'/portfolio'} ><div
-                                className='group relative'
+                                className='group our-work-div '
                                 key={index}
                                 id={`hovered-item-${index}`}
                                 onMouseEnter={() => handleMouseEnter(index)}
@@ -197,13 +98,13 @@ const OurWork = () => {
                                 onMouseMove={handleMouseMove}
                             >
                                 <motion.li
-                                    className='backdrop-blur-sm flex justify-between text-5xl lg:text-7xl px-5 py-10 transition-all duration-500 bg-[#ffffff67] rounded-full my-5 hover:tracking-wider'
+                                    className='backdrop-blur-sm flex justify-between text-5xl lg:text-7xl md:px-5 py-10 transition-all duration-500 bg-[#ffffff67] rounded-full my-5 hover:tracking-wider'
                                     variants={listItemVariants}
                                     transition={{ delay: 0.2 * index, duration: 0.5, ease: 'easeIn' }}
                                 >
                                     <p className='px-10'>{item}</p>
                                     <motion.img
-                                        className='z-50  rounded-3xl hidden group-hover:block absolute right-1/2 bottom-0 w-64 h-56 float-right object-cover'
+                                        className='z-50 rounded-lg md:rounded-2xl lg:rounded-3xl hidden group-hover:block absolute right-1/2 bottom-0 w-24 h-16 sm:w-28 sm:h-20 md:w-44 md:h-36  lg:w-64 lg:h-56 float-right object-cover'
                                         src={item === 'Animations' ? animation : item === 'Illustrations' ? illustration : item === 'Comics' ? comics : item === 'Emotes' ? emotes : null}
                                         alt={item}
                                         style={{
